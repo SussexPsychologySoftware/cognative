ec7d9cc5499041f6d36a7e4029bbe2743fc7d5ab
Max Lovell <max_lovell@hotmail.co.uk>
2025-10-14 18:19:21 +0100

    Multiline text input component with done button

---
+++ components/MultilineTextInput.tsx
@@ -0,0 +1,50 @@
+import {InputAccessoryView, Keyboard, Platform, Pressable, TextInput, View, Text} from "react-native";
+import {globalStyles} from "@/styles/appStyles";
+
+export default function MultilineTextInput({ value, placeholder, onChange }: { value: string, placeholder?: string, onChange: (number: string) => void}){
+    // controlled component, no internal state
+    const inputAccessoryViewID = 'uniqueID'; // required for adding the done button back in
+
+    function handleInput(response: string) {
+        onChange(response);
+    }
+
+    return (
+        <>
+            <TextInput
+                multiline
+                value={value}
+                placeholder={placeholder}
+                placeholderTextColor={'grey'}
+                style={globalStyles.input}
+                onChangeText={text => handleInput(text)}
+                // returnKeyType='done' // This changes the 'enter' key, and no done button present by default
+                inputAccessoryViewID={inputAccessoryViewID}
+            />
+            { // Include custom done button in top bar on iOS
+                Platform.OS === 'ios' &&
+                <InputAccessoryView nativeID={inputAccessoryViewID}
+                                    style={{backgroundColor: 'white'}}
+                >
+                    <View style={{
+                        backgroundColor: '#c8cbcd', // Roughly iOS ish for now TODO: improve
+                    }}
+                    >
+                        <Pressable onPress={()=>Keyboard.dismiss()}>
+                            <Text style={{
+                                color: '#007AFF',
+                                fontSize: 17,
+                                fontWeight: '500',
+                                textAlign: 'right',
+                                padding: 12
+                            }}
+                            >
+                                Done
+                            </Text>
+                        </Pressable>
+                    </View>
+                </InputAccessoryView>
+            }
+        </>
+    )
+}
