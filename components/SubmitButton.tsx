138ff0da2e2de93d05cea0d27fcfae5a15c238f8
Max Lovell <max_lovell@hotmail.co.uk>
2025-10-14 19:12:18 +0100

    Submit button component

---
+++ components/SubmitButton.tsx
@@ -0,0 +1,42 @@
+import React, {useState} from 'react'
+import {Text, StyleSheet, Pressable} from 'react-native'
+
+export default function SubmitButton({ text, disabledText, disabled, onPress, style } : { text: string, disabledText?: string, disabled: boolean, onPress: () => void | Promise<void>, style?: object }) {
+    const [pressExecuting, setPressExecuting] = useState(false); // Define pressExecuting state
+
+    const handlePress = async () => {
+        // This makes the disabled function a little tighter but not air-tight
+            // Good for if the function wants to e.g. prevent multiple submissions
+            // Consider the debouncing function in parent if looking to stop multiple presses quicker
+        if (pressExecuting) return;
+        setPressExecuting(true);
+        try {
+            await onPress(); // Just doesn't do anything if synchronous function
+        } catch (error) {
+            console.error('Error executing press: ', error);
+        } finally {
+            setPressExecuting(false);
+        }
+    };
+
+    return(
+        <Pressable disabled={disabled} onPress={handlePress} style={[{backgroundColor: disabled?'grey':'white'}, styles.button, style]}>
+            <Text style={styles.text}>{ disabled ? (disabledText??text) : text}</Text>
+        </Pressable>
+    )
+}
+
+const styles = StyleSheet.create({
+    button: {
+        color: 'black',
+        paddingVertical: 10,
+        paddingHorizontal: 15,
+        borderRadius: 5,
+        alignSelf: 'center'
+    },
+    text: {
+        color: 'black',
+        justifyContent: 'center'
+    }
+})
+
