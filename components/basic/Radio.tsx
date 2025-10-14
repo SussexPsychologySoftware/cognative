86c8c9ea3ec61e59381197ea61b2a09f86a81880
Max Lovell <max_lovell@hotmail.co.uk>
2025-10-14 21:07:55 +0100

    Add radio button

---
+++ components/basic/Radio.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { MaterialIcons } from '@expo/vector-icons';
+import { View } from 'react-native'
+import { colours } from "@/styles/appStyles";
+
+export default function Radio({ selected, colour }: {
+    selected: boolean,
+    colour?: string
+}) {
+    return(
+            <MaterialIcons
+                name={selected ? 'radio-button-on' : 'radio-button-off'}
+                size={30}
+                color={colour ?? colours.primary}
+            />
+    )
+}
