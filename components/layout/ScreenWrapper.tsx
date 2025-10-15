beec08035fcbdf842081b9f420c8ef8b4be6cd2f
Max Lovell <max_lovell@hotmail.co.uk>
2025-10-15 09:12:49 +0100

    Add screen wrapper component for screen defaults

---
+++ components/layout/ScreenWrapper.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import {
+    KeyboardAvoidingView,
+    ScrollView,
+    Platform,
+    View
+} from 'react-native';
+import {SafeAreaView} from "react-native-safe-area-context";
+import { StatusBar } from 'expo-status-bar';
+import { Stack } from 'expo-router';
+
+export const ScreenWrapper = ({
+                                  children,
+                                  scrollable = true,
+                                  statusBarStyle = 'dark',
+                                  keyboardBehavior = Platform.OS === 'ios' ? 'padding' : 'height',
+                                  contentContainerStyle,
+                                  style,
+                                  safeAreaStyle,
+                              }:
+                              {
+                                  children?: any,
+                                  scrollable?: boolean,
+                                  statusBarStyle?: 'light'|'dark',
+                                  keyboardBehavior?: 'padding'|'height'|'position',
+                                  contentContainerStyle?: object,
+                                  style?: object,
+                                  safeAreaStyle?: object,
+                              }) => {
+
+    const content = scrollable ? (
+        <ScrollView
+            contentContainerStyle={contentContainerStyle}
+            keyboardShouldPersistTaps="handled"
+        >
+            {children}
+        </ScrollView>
+    ) : (
+        <View style={[{ flex: 1 }, style]}>
+            {children}
+        </View>
+    );
+
+    return (
+        <SafeAreaView style={[{ flex: 1 }, safeAreaStyle]}>
+            <Stack.Screen options={{ headerShown: false }} />
+            <KeyboardAvoidingView
+                behavior={keyboardBehavior}
+                style={{ flex: 1 }}
+            >
+                <StatusBar style={statusBarStyle}/>
+                {content}
+            </KeyboardAvoidingView>
+        </SafeAreaView>
+    );
+};
