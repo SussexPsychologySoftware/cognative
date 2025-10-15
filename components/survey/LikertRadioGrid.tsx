8945fefcced946d1d2c4afc51e06639fbcfb1e40
Max Lovell <max_lovell@hotmail.co.uk>
2025-10-15 14:56:00 +0100

    Radio grid simplifications and styling

---
+++ components/survey/LikertRadioGrid.tsx
@@ -0,0 +1,205 @@
+import { View, Pressable, Text, StyleSheet } from 'react-native';
+import { globalStyles } from "@/styles/appStyles";
+import Hypher from 'hypher';
+import english from 'hyphenation.en-gb';
+import Radio from "@/components/basic/Radio";
+
+const h = new Hypher(english);
+
+function hyphenate(text: string) {
+    return text?.split(/(\s+)/).map(part =>
+        part.trim() ? h.hyphenate(part).join('\u00AD') : part
+    ).join('') ?? text;
+}
+
+// Define single radio button component
+export function OptionLabels({options, hasQuestion, oneWordPerLine}: { options: string[], hasQuestion?: boolean, oneWordPerLine?: boolean }) {
+    return (
+        <View style={[styles.row, styles.optionsTextContainer]} >
+            {hasQuestion && <Text style={[styles.gridItem, styles.question]}></Text>}
+            {options.map((option, index) => {
+                //https://stackoverflow.com/questions/57578586/control-word-text-break-on-react-native-text-element
+                return(
+                    <Text
+                        key={`label-${option}`}
+                        android_hyphenationFrequency='full'
+                        allowFontScaling={true}
+                        adjustsFontSizeToFit={true}
+                        numberOfLines={oneWordPerLine ? option.split(' ').length : 0}
+                        style={[globalStyles.whiteText, styles.optionsText, styles.gridItem]}
+                    >
+                        {hyphenate(option)}
+                    </Text>
+                )
+            })}
+        </View>
+    );
+}
+
+function SecondaryLabels({labels, hasQuestion}: { labels: string[], hasQuestion?: boolean }) {
+    return (
+        <View style={[styles.row, styles.optionsTextContainer]}>
+            {hasQuestion && <Text style={[styles.gridItem,styles.question]}></Text>}
+            {labels.map((label, index) => (
+                <View key={`secondary-${index}`} style={[styles.gridItem, styles.secondaryLabelContainer]}>
+                    <Text style={[globalStyles.whiteText, styles.optionsText, styles.secondaryLabel]}>
+                        {label}
+                    </Text>
+                </View>
+            ))}
+        </View>
+    );
+}
+
+function RadioButton({ selected, onChange } : { selected: boolean, onChange: () => void }) {
+    return (
+        <Pressable style={[styles.gridItem, styles.radioButton]} onPress={onChange}>
+            <Radio selected={selected} />
+        </Pressable>
+    );
+}
+
+export function LikertQuestionRow(
+    {
+        selectedResponse,
+        question,
+        options,
+        onChange,
+        oddRow
+    }: {
+        selectedResponse: string,
+        question?: string,
+        options: string[],
+        onChange: (question: string, answer: string) => void,
+        oddRow?: boolean
+    }) {
+    return (
+        <View style={[styles.likertRow, styles.row, oddRow && styles.oddRow]}>
+            {
+                question &&
+                <Text style={[globalStyles.whiteText, styles.question, styles.gridItem]}>{question}</Text>
+            }
+            {
+                options.map((option, index) => (
+                    <RadioButton
+                        key={`radio-${option}`}
+                        selected={selectedResponse===option}
+                        onChange={() => {onChange(question??'', option)}}
+                    />
+                ))
+            }
+        </View>
+    );
+}
+
+export default function LikertRadioGrid(
+    {
+        responses,
+        questions,
+        options,
+        onChange,
+        secondaryLabels,
+        oneWordPerLine,
+        headerRepeatInterval=0
+    }:
+    {
+        responses: Record<string, string>,
+        questions?: string[],
+        options: string[],
+        onChange: (question: string, answer: string) => void,
+        secondaryLabels?: string[],
+        headerRepeatInterval?: number,
+        oneWordPerLine?: boolean
+    }) {
+
+    return (
+        <View style={[styles.radioGrid, {paddingHorizontal: secondaryLabels ? '5%' : null}]}>
+            {
+                [
+                    secondaryLabels && (
+                        <SecondaryLabels
+                            key="secondary-labels"
+                            labels={secondaryLabels}
+                            hasQuestion={!!questions}
+                        />
+                    ),
+                    ...(questions?.flatMap((question, i) => [
+                        (i === 0 || i % headerRepeatInterval === 0) && (
+                            <OptionLabels
+                                key={`options-${i}`}
+                                options={options}
+                                oneWordPerLine={oneWordPerLine}
+                                hasQuestion={!!questions} />
+                        ),
+                        <LikertQuestionRow
+                            key={`question-${i}`}
+                            selectedResponse={responses[question]}
+                            question={question}
+                            options={options}
+                            onChange={onChange}
+                            oddRow={i%2===1}
+                        />
+                    ]).filter(Boolean) ?? [])
+                ].filter(Boolean)
+            }
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    question: {
+        fontSize: 17,
+        fontStyle: 'italic',
+        minWidth: 50,
+    },
+
+    // Grid container and rows
+    radioGrid: {
+        rowGap: 20,
+        // STYLING??
+        backgroundColor: '#d3d3d3',
+        borderWidth: 1,
+        borderColor: 'darkgrey',
+        borderRadius: 20,
+        padding: 15
+    },
+    row: { // Radio Row applies to each row
+        justifyContent: 'space-between',
+        // width: '100%',
+        flexDirection: 'row',
+        columnGap: 10,
+    },
+    likertRow: {
+        padding: 5,
+        borderRadius: 5,
+    },
+    gridItem: {
+        flex: 1, // flex everything
+    },
+    // options
+    optionsTextContainer: { // Headers at bottom of container
+        marginHorizontal: 15,
+    },
+    oddRow: {
+        backgroundColor: '#b0c4dd',
+    },
+    optionsText: {
+        textAlign: 'center',
+        fontSize: 16,
+        fontWeight: "500",
+    },
+
+    // Secondary labels
+    secondaryLabelContainer: {
+        alignItems: 'center',
+    },
+    secondaryLabel: {
+        width: 70, // Fixed width larger than container
+    },
+
+    // Radio buttons
+    radioButton: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
