6f3fc05f78a4e4b8a8864b75b49468b25b5fe624
Max Lovell <max_lovell@hotmail.co.uk>
2025-10-15 09:12:27 +0100

    Add scripts and types for converting between colour spaces

---
+++ types/colours.ts
@@ -0,0 +1,26 @@
+export interface LAB {
+    l: number;
+    a: number;
+    b: number;
+}
+
+export interface RGB {
+    r: number;
+    g: number;
+    b: number;
+}
+
+export interface LCH {
+    l: number;
+    c: number;
+    h: number;
+}
+
+export interface XYZ {
+    x: number;
+    y: number;
+    z: number;
+}
+
+export interface Range { min: number, max: number }
+
