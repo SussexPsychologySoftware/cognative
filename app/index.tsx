00fedeef739feae469307295532d3e15be22736e
Max Lovell <max_lovell@hotmail.co.uk>
2025-10-15 11:18:18 +0100

    Add useSurvey hook to index page survey

--- app/index.tsx
+++ app/index.tsx
@@ -3,6 +3,7 @@ import NumericInput from "@/components/basic/NumericInput";
 import React, {useState} from "react";
 import MultilineTextInput from "@/components/basic/MultilineTextInput";
 import TimePicker from "@/components/basic/TimePicker";
+
 import SubmitButton from "@/components/basic/SubmitButton";
 import RadioList from "@/components/survey/RadioList";
 import AudioPlayer from "@/components/media/AudioPlayer";
@@ -10,33 +11,30 @@ import {ScreenWrapper} from "@/components/layout/ScreenWrapper";
 import {StatusBar} from "expo-status-bar";
 import {globalStyles} from "@/styles/appStyles";
 
+import {useSurvey} from "@/hooks/useSurvey";
+
 export default function Index() {
-    const [responses, setResponses] = useState({
-        numericInput: '',
-        multilineTextInput: '',
-        radioOn: false,
-        radioList: '',
-        timePicker: new Date()
-    });
-    const [submitting, setSubmitting] = useState(false);
+    const questions  = [
+        {
+            question: 'numericInput',
+            required: true,
+            default: '',
+        },
+        {
+            question: 'multilineTextInput',
+        },
+        {
+            question: 'radioList',
+        },
+        {
+            question: 'timePicker',
+            default: new Date(),
+        },
+    ]
+
     const [playingAudio, setPlayingAudio] = useState(false);
 
-
-    function updateResponses(newValue: any, responseField: string) {
-        setResponses({...responses, [responseField]: newValue})
-    }
-
-    function handleSubmit(){
-        if(submitting) return;
-        try {
-            setSubmitting(true);
-            Alert.alert('submitted',JSON.stringify(responses))
-        } catch (e) {
-            console.error(e)
-        } finally {
-            setSubmitting(false);
-        }
-    }
+    const { responses, updateResponses, handleSurveySubmit, warning, isSubmitting, progress, resetSurvey } = useSurvey(questions);
 
     return (
         <ScreenWrapper
@@ -45,24 +43,25 @@ export default function Index() {
         >
             <Text style={[globalStyles.pageTitle,{marginVertical: 30}]}>Survey Example</Text>
             <StatusBar style={'dark'}/>
+            <Text style={globalStyles.standardText}>{progress}</Text>
             <View style={styles.inputsContainer}>
                 <NumericInput
                     value={responses.numericInput}
                     placeholder={"Enter numeric number"}
-                    onChange={(newValue: string) => {updateResponses(newValue,'numericInput')}}
+                    onChange={(newValue: string) => {updateResponses('numericInput', newValue)}}
                 />
                 <MultilineTextInput
                     value={responses.multilineTextInput}
                     placeholder={"Multiline text input"}
-                    onChange={(newValue: string) => {updateResponses(newValue,'multilineTextInput')}}
+                    onChange={(newValue: string) => {updateResponses('multilineTextInput', newValue)}}
                 />
                 <TimePicker
                     value={responses.timePicker}
-                    onChange={(newValue: Date|null) => {updateResponses(newValue,'timePicker')}}
+                    onChange={(newValue: Date|null) => {updateResponses('timePicker', newValue)}}
                 />
                 <RadioList
                     options={['Yes', 'No']}
-                    onSelect={(response: string) => {updateResponses(response, 'radioList')}}
+                    onSelect={(response: string) => {updateResponses('radioList', response)}}
                     containerStyle={{'width': '60%'}}
                 />
                 <AudioPlayer   audioSource={require('../assets/sounds/binaural.mp3')}
@@ -70,10 +69,17 @@ export default function Index() {
                                onPress={()=>{setPlayingAudio(!playingAudio)}}
                                volume={1}
                 />
+                <Text style={globalStyles.warning}>{warning}</Text>
                 <SubmitButton
-                    onPress={handleSubmit}
+                    onPress={async()=>{await handleSurveySubmit()}}
                     text={"Submit"}
                     disabledText={"Submitting..."}
+                    disabled={isSubmitting}
+                />
+                <SubmitButton
+                    onPress={()=>{resetSurvey()}}
+                    text={"Reset Survey"}
+                    disabledText={"Resetting..."}
                     disabled={false}
                 />
             </View>
