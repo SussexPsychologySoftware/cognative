5f7895ace478fa162a8c3f9bec6ee2f29d2d1df4
Max Lovell <max_lovell@hotmail.co.uk>
2025-10-20 14:05:39 +0100

    Pagelist component for debugging

---
+++ components/debug/PageList.tsx
@@ -0,0 +1,44 @@
+import {StyleSheet, View, Text} from "react-native";
+import {globalStyles} from "@/styles/appStyles";
+import {Link, RelativePathString} from "expo-router";
+
+export default function PageList({ pages }: { pages?: string[]}){
+
+    if(!pages) pages = ['/'] // TODO: add pages here
+    return (
+        <View style={styles.pageList}>
+            <Text style={styles.debugTitle}>Page List:</Text>
+            {
+                pages.map((page,i) =>
+                    <Link
+                        key={page}
+                        href={"/"+page as RelativePathString}
+                        style={[styles.debugLink]}
+                    >
+                        {page !== '' ? page : 'home'}
+                    </Link>
+                )
+            }
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    // Page list for debugging
+    pageList: {
+        gap: 5,
+            paddingTop: 20,
+            borderTopWidth: 1,
+            borderTopColor: 'grey',
+    },
+    debugTitle: {
+        color: 'grey',
+            fontSize: 14,
+            marginBottom: 8,
+    },
+    debugLink: {
+        color: 'grey',
+            fontSize: 14,
+            textDecorationLine: 'underline',
+    },
+});
