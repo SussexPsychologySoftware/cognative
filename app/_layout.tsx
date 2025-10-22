8b554309fe640ea653e9ef961ec44c53fe23ae7d
Max Lovell <max_lovell@hotmail.co.uk>
2025-10-22 09:05:30 +0100

    Explicitly list current index and survey pages in layout screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,14 @@
 import { Stack } from "expo-router";
+import {StatusBar} from "expo-status-bar";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+      <>
+        <StatusBar style="light" />
+        <Stack>
+          <Stack.Screen name="index" options={{ title: 'Home' }} />
+          <Stack.Screen name="surveyExample" options={{ title: 'Example Survey' }} />
+        </Stack>
+      </>
+  )
 }
