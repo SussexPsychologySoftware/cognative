95f624dd66ee8ea72f2d2bb97869a3fd9069b295
Max-Lovell <max_lovell@hotmail.co.uk>
2025-10-24 10:42:41 +0100

    Add experiment context provider to root of app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,18 +3,22 @@ import {StatusBar} from "expo-status-bar";
 import {GestureHandlerRootView} from "react-native-gesture-handler";
 import {dataQueue} from "@/services/data/dataQueue";
 import {useRestore} from "@/hooks/useRestore";
+import React from "react";
+import {ExperimentProvider} from "@/context/ExperimentContext";
 
 export default function RootLayout() {
     // void dataQueue.processQueue(); // To process queue on app load
     //useRestore(); // Restore app to specific screen when opened
 
     return (
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <StatusBar style="light" />
-        <Stack>
-          <Stack.Screen name="index" options={{ title: 'Home' }} />
-          <Stack.Screen name="surveyExample" options={{ title: 'Example Survey' }} />
-        </Stack>
-      </GestureHandlerRootView>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <ExperimentProvider>
+                <StatusBar style="light" />
+                <Stack>
+                    <Stack.Screen name="index" options={{ title: 'Home' }} />
+                    <Stack.Screen name="surveyExample" options={{ title: 'Example Survey' }} />
+                </Stack>
+            </ExperimentProvider>
+        </GestureHandlerRootView>
   )
 }
