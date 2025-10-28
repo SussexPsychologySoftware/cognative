71a22b73e374cfef534937e86ac4d7b9063bd6ca
Max Lovell <max_lovell@hotmail.co.uk>
2025-10-28 12:09:04 +0000

    List new dynamic survey screen in layout

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -6,6 +6,7 @@ export default function AppLayout() {
     return (
         <Stack>
             <Stack.Screen name="index" options={{ title: 'Home' }} />
+            <Stack.Screen name="survey" options={{ title: 'Survey' }} />
             <Stack.Screen name="surveyExample" options={{ title: 'Survey' }} />
             <Stack.Screen name="end" options={{ title: 'Finished' }} />
         </Stack>
