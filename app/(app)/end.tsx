e18ebc9b77c3ae3affeedbf9ea3c4cb093882220
Max Lovell <max_lovell@hotmail.co.uk>
2025-10-31 16:41:36 +0000

    Spruce up end screen

--- app/(app)/end.tsx
+++ app/(app)/end.tsx
@@ -1,14 +1,26 @@
 import {Text, View, StyleSheet} from 'react-native';
 import ResetButtons from "@/components/debug/ResetButtons";
 import PageList from "@/components/debug/PageList";
+import {useExperiment} from "@/context/ExperimentContext";
+import {globalStyles} from "@/styles/appStyles";
+import {StandardView} from "@/components/layout/StandardView";
 
 export default function EndScreen() {
+    const { definition, displayState, state } = useExperiment();
+
+    // TODO: add experimenter contact to config?
     return (
-        <View style={styles.container}>
-            <Text>The experiment is now over</Text>
+        <StandardView
+            contentContainerStyle={{
+                marginVertical: 40,
+                gap: 30
+        }}
+        >
+            <Text style={globalStyles.standardText}>The experiment is now over, please contact the experimenter.</Text>
+            <Text style={globalStyles.standardText}>Your Participant ID is: {state?.participantId}</Text>
             <ResetButtons/>
             <PageList/>
-        </View>
+        </StandardView>
     );
 }
 
@@ -16,29 +28,5 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 20,
-        backgroundColor: '#25292e',
-        // alignItems: 'center',
-        justifyContent: 'center',
-    },
-    resetButtons: {
-        marginVertical: 40,
-        gap: 10
-    },
-    // Page list for debugging
-    pageList: {
-        gap: 5,
-        paddingTop: 20,
-        borderTopWidth: 1,
-        borderTopColor: 'grey',
-    },
-    debugTitle: {
-        color: 'grey',
-        fontSize: 14,
-        marginBottom: 8,
-    },
-    debugLink: {
-        color: 'grey',
-        fontSize: 14,
-        textDecorationLine: 'underline',
     },
 });
