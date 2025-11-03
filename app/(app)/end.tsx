7c6f351532c1c8aba940b21a60ab8fee8c75f928
Max Lovell <max_lovell@hotmail.co.uk>
2025-11-03 20:19:45 +0000

    Add Debug list to end screen

--- app/(app)/end.tsx
+++ app/(app)/end.tsx
@@ -1,9 +1,10 @@
-import {Text, View, StyleSheet} from 'react-native';
+import {Text} from 'react-native';
 import ResetButtons from "@/components/debug/ResetButtons";
 import PageList from "@/components/debug/PageList";
 import {useExperiment} from "@/context/ExperimentContext";
 import {globalStyles} from "@/styles/appStyles";
 import {StandardView} from "@/components/layout/StandardView";
+import Debug from "@/components/debug/Debug";
 
 export default function EndScreen() {
     const { state } = useExperiment();
@@ -20,8 +21,7 @@ export default function EndScreen() {
         >
             <Text style={globalStyles.standardText}>The experiment is now over, please contact the experimenter.</Text>
             <Text style={globalStyles.standardText}>Your Participant ID is: {state?.participantId}</Text>
-            <ResetButtons/>
-            <PageList/>
+            <Debug/>
         </StandardView>
     );
 }
