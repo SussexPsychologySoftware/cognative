7a65050e9233dee85430634f4ae6a6c232749a27
Max Lovell <max_lovell@hotmail.co.uk>
2025-11-03 20:19:35 +0000

    Add view around debug list to stop main page layout affecting children

--- components/debug/Debug.tsx
+++ components/debug/Debug.tsx
@@ -1,15 +1,16 @@
 import ResetButtons from "@/components/debug/ResetButtons";
 import PageList from "@/components/debug/PageList";
 import ExperimentInfo from "@/components/debug/ExperimentInfo";
+import {View} from "react-native";
 
 export default function Debug({object}:{object?: object}) {
     return (
-        <>
+        <View>
             <ResetButtons/>
             <PageList/>
             <ExperimentInfo
                 object={object}
             />
-        </>
+        </View>
     )
 }
