edc92d94da37635d3484faa2ea927c7a72091ca0
Max Lovell <max_lovell@hotmail.co.uk>
2025-11-07 14:44:46 +0000

    Get Images to fit on page better

--- components/media/Picture.tsx
+++ components/media/Picture.tsx
@@ -1,15 +1,36 @@
-import {StyleSheet, View, Text} from 'react-native';
-import {Image} from 'expo-image'
+import {StyleSheet, View, Text, Dimensions, useWindowDimensions} from 'react-native';
+import {Image, ImageLoadEventData} from 'expo-image'
+import {useState} from "react";
 
 export default function Picture({asset, caption, width, height, imageStyle, captionStyle}: {asset: number, caption?: string, width?: number, height?: number, imageStyle?: object, captionStyle?: object}) {
     // e.g. asset = require('../assets/images/screens.jpg')
+
+    // Note if image has no height onLoad doesn't fire, so initial state needs to be 1 to calculate proper aspectRatio
+    const [aspectRatio, setAspectRatio] = useState<number>(1);
+    const { width: windowWidth, height: windowHeight } = useWindowDimensions();
+    const screenRatio = windowWidth / windowHeight;
+
+    console.log('aspectRatio', aspectRatio);
     return (
         <View style={styles.imageContainer}>
             <Image
-                style={[styles.image, {width: width ?? '100%'}, {height: height ?? 300}, imageStyle]}
+                style={[
+                    styles.image,
+                    // This doesn't work 100% but good enough for now.
+                    aspectRatio > screenRatio ? {width: '100%'} : {height: windowHeight-100},
+                    {aspectRatio},
+                    width!==undefined && {width: width},
+                    height!==undefined && {height: height},
+                    imageStyle
+                ]}
                 source={asset}
                 placeholder={'Image missing'}
                 contentFit="contain"
+                onLoad={(event: ImageLoadEventData) => {
+                    const { width, height } = event.source;
+                    setAspectRatio(width / height);
+                    console.log({ width, height, aspectRatio });
+                }}
             />
             {
                 caption &&
@@ -25,12 +46,11 @@ export default function Picture({asset, caption, width, height, imageStyle, capt
 const styles = StyleSheet.create({
     imageContainer: {
         alignItems: 'center',
-        // maxWidth: '100%',
-        // maxHeight: '100%',
-        gap: 5
+        gap: 10,
+        // maxHeight: Dimensions.get('window').height,
     },
     image: {
-        // maxWidth: '100%',
+        width: '100%',
         // maxHeight: '100%',
     },
     imageCaption: {
@@ -38,7 +58,6 @@ const styles = StyleSheet.create({
         fontStyle: 'italic',
         textAlign: 'center',
         color: '#666',
-        // paddingHorizontal: 30,
     },
 })
 
