58a649c35e5eac5937e2528289272ef12c760bb3
Max Lovell <max_lovell@hotmail.co.uk>
2025-11-14 14:46:37 +0000

    New show trial data page

--- app/(app)/DSA/showTrialData.tsx
+++ app/(app)/DSA/showTrialData.tsx
@@ -1,10 +1,13 @@
-import {Text, View, StyleSheet, Pressable} from "react-native";
+import {Text, View, StyleSheet, Pressable, ScrollView, ColorValue} from "react-native";
 import React, {useEffect, useState} from "react";
 import {LAB, LCH, RGB} from "@/types/colours";
 import {useExperiment} from "@/context/ExperimentContext";
-import {StandardView} from "@/components/layout/StandardView";
 import {DataService} from "@/services/data/DataService";
-import {globalStyles} from "@/styles/appStyles";
+import MunsellChip from "@/components/DSA/MunsellChip";
+import {SafeAreaView} from "react-native-safe-area-context";
+import {StatusBar} from "expo-status-bar";
+import Debug from "@/components/debug/Debug";
+import DebugButtons from "@/components/debug/DebugButtons";
 // Return selected colour,
 // overthinking, maybe just pass in the update and toggle functions? horizontal?
 
@@ -26,31 +29,45 @@ interface Trial {
 
 export default function ShowTrialDataScreen() {
     const { displayState, getTaskFilename } = useExperiment();
-
     const [trialData, setTrialData] = useState<Trial[]>([]);
     const [selectedIndex, setSelectedIndex] = useState(0);
     const testData: TestColour[] = [
+        {RGB: {r:255, g:255, b:255}, description: 'WHITE'},
         {RGB: {r:255, g:0, b:0}, description: 'RED'},
+        {RGB: {r:50, g:0, b:0}, description: 'RED (50)'},
+        {RGB: {r:100, g:0, b:0}, description: 'RED (100)'},
+        {RGB: {r:150, g:0, b:0}, description: 'RED (150)'},
+        {RGB: {r:200, g:0, b:0}, description: 'RED (200)'},
         {RGB: {r:0, g:255, b:0}, description: 'GREEN'},
+        {RGB: {r:0, g:50, b:0}, description: 'GREEN (50)'},
+        {RGB: {r:0, g:100, b:0}, description: 'GREEN (100)'},
+        {RGB: {r:0, g:150, b:0}, description: 'GREEN (150)'},
+        {RGB: {r:0, g:200, b:0}, description: 'GREEN (200)'},
         {RGB: {r:0, g:0, b:255}, description: 'BLUE'},
-        {RGB: {r:255, g:255, b:0}, description: 'YELLOW'},
-        {RGB: {r:255, g:255, b:255}, description: 'WHITE'}
-    ]
-    const [backgroundColour, setBackgroundColour] = useState<RGB>(testData[0].RGB);
-    const [consentData, setConsentData] = useState<Record<string, any>>({});
+        {RGB: {r:0, g:0, b:50}, description: 'BLUE (50)'},
+        {RGB: {r:0, g:0, b:100}, description: 'BLUE (100)'},
+        {RGB: {r:0, g:0, b:150}, description: 'BLUE (150)'},
+        {RGB: {r:0, g:0, b:200}, description: 'BLUE (200)'},
+        {RGB: {r:255, g:255, b:0}, description: 'YELLOW'}
+    ];
+    const RGB2rgb = (rgb: RGB) => {
+        return `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`;
+    }
+    const [backgroundColour, setBackgroundColour] = useState<ColorValue>(RGB2rgb(testData[0].RGB));
+    const [infoData, setInfoData] = useState<Record<string, any>>({});
 
     useEffect(() => {
         const loadPreviousData = async () => {
             try {
                 // Consent
-                const consentFilename = getTaskFilename('consent')
-                if(!consentFilename) return null;
-                const consentData = await DataService.getData(consentFilename)
-                if (consentData === null) return null;
+                const infoFilename = getTaskFilename('setup')
+                if(!infoFilename) return null;
+                const participantInfo = await DataService.getData(infoFilename)
+                if (participantInfo === null) return null;
                 // const consentDataFiltered = Object.fromEntries(
-                //     Object.entries(consentData).filter(([_, v]) => v)
+                //     Object.entries(infoData).filter(([_, v]) => v)
                 // )
-                setConsentData(consentData);
+                setInfoData(participantInfo);
                 // Trials
                 const trialsFilename = getTaskFilename('adjust')
                 if(!trialsFilename) return null;
@@ -68,17 +85,25 @@ export default function ShowTrialDataScreen() {
 
 
     const handlePress = (responseColour: RGB, selectedIndex: number) => {
-        setBackgroundColour(responseColour);
+        setBackgroundColour(RGB2rgb(responseColour));
         setSelectedIndex(selectedIndex);
     }
 
     function DisplayColourButton({index, RGB, targetColour, displayIndex}: {index: number, RGB: RGB, targetColour?: string, displayIndex?: number }) {
         return (
             <Pressable
-                style={[styles.trialSelector, selectedIndex===index && styles.selectedTrial]}
+                style={[
+                    styles.trialSelector,
+                    selectedIndex===index && {backgroundColor: backgroundColour},
+                    {borderColor: backgroundColour}
+                ]}
                 onPress={()=>handlePress(RGB, index)}>
                 <Text
-                    style={styles.text}
+                    style={[
+                        styles.text,
+                        {color: backgroundColour},
+                        selectedIndex===index && {color: 'black'},
+                    ]}
                 >
                     {displayIndex??index}) {RGB.r}, {RGB.g}, {RGB.b} {targetColour && `| ${targetColour}`}
                 </Text>
@@ -87,19 +112,20 @@ export default function ShowTrialDataScreen() {
     }
 
     return (
-            <StandardView
-                safeAreaStyle={{
-                    backgroundColor: `rgb(${backgroundColour.r}, ${backgroundColour.g}, ${backgroundColour.b})`
-                }}
-            >
-                {/*<Text style={{color: 'black', fontSize: 20, marginBottom: 5, fontWeight: 'bold'}}>ID: {displayState?.participantId}</Text>*/}
-                <Text
-                    selectable={true}
-                    style={[styles.text, {marginVertical: 40}]}>
-                    Consent Form: {JSON.stringify(consentData,null,4)}
-                </Text>
-                <View style={styles.trialList}>
-                    <Text style={styles.text}>TEST COLOURS</Text>
+        <SafeAreaView
+            style={styles.container}
+        >
+            <StatusBar style={'dark'}/>
+            <ScrollView
+                    style={[styles.trialList, {borderColor: backgroundColour}]}
+                    contentContainerStyle={[styles.trialListContent, {borderColor: backgroundColour}]}
+                >
+                    <Text
+                        selectable={true}
+                        style={[styles.text, {color: backgroundColour, marginVertical: 40}]}>
+                        Info: {JSON.stringify(infoData,null,4)}
+                    </Text>
+                    <Text style={[styles.text, {color: backgroundColour}]}>TEST COLOURS</Text>
                     {
                         testData.map((item, index) =>{
                             return(
@@ -112,8 +138,8 @@ export default function ShowTrialDataScreen() {
                                 />)
                         })
                     }
-                    <Text style={styles.text}>TRIAL DATA</Text>
-                    { trialData && 
+                    <Text style={[styles.text,{color: backgroundColour}]}>TRIAL DATA</Text>
+                    { trialData &&
                         trialData.map((item, index) => {
                             // console.log(item);
                             return(
@@ -125,52 +151,58 @@ export default function ShowTrialDataScreen() {
                                 />
                             )
 
-                        }
-
-                        )
+                        })
                     }
+                <DebugButtons buttonRowStyle={{flexDirection: 'column'}}/>
+                </ScrollView>
+                <View style={styles.munsellChip}>
+                    <MunsellChip
+                        color={backgroundColour}
+                    />
                 </View>
-            </StandardView>
+        </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
-    buttons:{
-        alignItems: "flex-start",
-        flexDirection: "column",
-        gap: 10
-    },
-    button: {
-        backgroundColor: 'black',
-        alignSelf: 'flex-start'
-    },
-
     container: {
-        padding: 30,
-        alignItems: "flex-start",
-        minHeight: '100%'
+        backgroundColor: 'black',
+        flexDirection: 'row',
     },
     trialList: {
+        maxWidth: '40%',
+        // borderWidth: 1,
+        flex: 1,
+    },
+    trialListContent: {
         justifyContent: "center",
         alignItems: "flex-start",
         gap: 10,
-        marginBottom: 50,
+        margin: 10,
+        borderWidth: 1,
+        padding: 10,
     },
     trialSelector: {
         borderRadius: 10,
         borderWidth: 1,
-        borderStyle: "solid",
-        borderColor: "black",
+        // borderColor: "lightgrey",
         padding: 10,
     },
     selectedTrial: {
-        backgroundColor: "darkgrey",
+        // backgroundColor: "darkgrey",
     },
     text: {
         fontSize: 15,
         fontWeight: "bold",
-        color: "black",
+        color: "lightgrey",
     },
+    munsellChip: {
+        alignItems: "center",
+        justifyContent: "center",
+        flex: 1,
+        // borderWidth: 1,
+        // borderColor: "lightgrey",
+    }
 
 });
 
